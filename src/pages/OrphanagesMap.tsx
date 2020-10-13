5b279b96409d52084eae98de12e083b8fcabc1ec
Juniel Dantas <juniel.dantas@gmail.com>
2020-10-13 01:21:52 -0300

    Start commit

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <Link to="/">
+                        <img src={mapMarkerImg} alt="Happy" />
+                    </Link>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Brasilia</strong>
+                    <span>Distrito Federal</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-15.7744228, -48.0772735]}
+                zoom={12}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer
+                    // url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianVuaWVsZGFudGFzIiwiYSI6ImNrZzdlcnZxdDA0dW0yeXBkOW9zM3R4a3QifQ.GY_tSVGe2W_u12hqlyEhEw`}
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+            <Link to="/create" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+
+        </div>
+    );
+}
+
+export default OrphanagesMap;
